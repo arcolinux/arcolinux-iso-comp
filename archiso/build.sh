a43c4f8c65ad79c8fee5aa708f7d243e60a324fe
Erik Dubois <erik.dubois@gmail.com>
2020-03-30 09:02:06 +0200

    new version new calamares new desktop

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinux-comp
-iso_label="arcolinux-comp-v20.3.1"
+iso_label="arcolinux-comp-v20.4.10"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.3.1"
+iso_version="v20.4.10"
 install_dir=arch
 work_dir=work
 out_dir=out
